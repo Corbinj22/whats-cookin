74a62254ca4aa05a3c999166dcac3cd92aceab7d
carflor <cfdeclet@gmail.com>
2020-03-26 17:07:09 -0600

    Finish user class method

--- src/user.js
+++ src/user.js
@@ -1,38 +1,30 @@
-const Pantry = require('../src/Pantry');
-const Recipe = require('../src/Recipe');
+const Pantry = require('../src/pantry');
+// const Recipe = require('../src/recipe');
 
 class User {
   constructor(obj) {
     this.name = obj.name;
     this.id = obj.id;
-    this.pantry = new Pantry(obj.pantry)
+    this.pantry = new Pantry(obj.pantry);
     this.favorites = [];
-    this.mealToCook = []
   }
 
-  addToFavorites() {
+  addToFavorites(recipe) {
     //adds recipe to the favorites array
+    this.favorites.push(recipe)
 
   }
 
   removeFromFavorites() {
+    // thinking we can validate if it pushes or takes out 
+    // within the add to favorites method
+
     // removes recipe from favorites array
   }
-
-
-  stageMealToCook() {
-    //take current meal selection
-    //pass it into pantry.validateMeal
-    //^ if above equates to true, move meal to meal to cook on user
-    //^ if aboce equates to false. run pantry required for meal and return required items.
-    //add meal user wants to cook to meal que.
-  }
-
-  cookMeal() {
-    //run remove item(s) from pantry
-    //set meal to cook to an empty []
-  }
-
 }
 
 module.exports = User;
+
+// if (typeof module !== ‘undefined’) {
+//   module.exports = User;
+// }
