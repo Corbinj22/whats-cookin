dd4fe3be4126966d7eb7834888b388d4610e6577
carflor <cfdeclet@gmail.com>
2020-03-31 22:29:28 -0600

    Sketch hide/toggle heart icon

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,10 @@ const homePage = document.querySelector('.home-page');
 const favoritesPage = document.querySelector('#favorites-page');
 const mealPage = document.querySelector('.meal-page');
 const mealContainer = document.getElementById('meal-container');
-const filterDropDown = document.querySelector('.type-selection')
+const filterDropDown = document.querySelector('.type-selection');
+const unfavorited = document.querySelector('#favorite-icon-inactive clickedIcon');
+const favorited = document.querySelector('#favorite-icon-active clickedIcon');
+
 let recipes;
 
 
@@ -43,6 +46,8 @@ function clickHandler(event) {
   event.target.classList.contains('food-img') ? displayMealPage(event) : null;
   event.target.classList.contains('favorite-icon-inactive') ? addMealToFavorites(target) : null;
 
+  event.target.classList.contains('favorite-icon-active') ? removeMealFromFavorites(target) : null;
+
   // if event target is favorites heart - call user method addtofaves
   // if card is already in add to faves - then PUSH OUT of faves and change heart
   // if event target is add BTN - call user method addMealToCook
@@ -67,8 +72,8 @@ function loadFavorites(recipe) {
       <img id="${recipe.id}" class="food-img" rel="food-img" src="${recipe.image}">
     </div>
     <div class="card-icon-container">
-      <img id="${recipe.id}" class="favorite-icon active hidden ${recipe.name}"src="https://img.icons8.com/color/96/000000/hearts.png"/>
-      <img id="${recipe.id}" class="favorite-icon inactive ${recipe.name}" src="https://img.icons8.com/windows/96/000000/hearts.png"/>
+      <img id="${recipe.id}" class="favorite-icon-active hidden ${recipe.name}"src="https://img.icons8.com/color/96/000000/hearts.png"/>
+      <img id="${recipe.id}" class="favorite-icon-inactive ${recipe.name}" src="https://img.icons8.com/windows/96/000000/hearts.png"/>
       <img id="${recipe.id}" class="icon cook-ready ${recipe.name}" src="https://img.icons8.com/doodle/96/000000/pot---v1.png"/>
     </div>
   </div>`
@@ -123,7 +128,8 @@ function displayHomePage() {
 // changed line 126 from this.user - to user
 function displayFavoritesPage() {
   favoritesPage.innerHTML = " ";
-  favorites = user.favorites.map(recipe => {
+  // favorites =     this is not needed
+  user.favorites.map(recipe => {
     favoritesPage.insertAdjacentHTML('afterbegin', loadFavorites(recipe));
   })
   favoritesPage.classList.remove('hidden');
@@ -139,6 +145,28 @@ function displayMealPage(event) {
   favoritesPage.classList.add('hidden');
 }
 
+function removeMealFromFavorites(target) {
+  let targetRecipe = recipes.find(recipe => {
+    return recipe.id == target.id
+  })
+  console.log('recipe picked', targetRecipe)
+  let targetValidate = user.favorites.find(meal => {
+    if (meal.id === targetRecipe.id) {
+      console.log('which meal', meal.id)
+      return false
+    }
+    console.log('Validate', targetValidate)
+    
+    if (!targetValidate) {  
+      
+      let repeated = user.favorites.indexOf('targetValidate');
+      user.favorites.splice(repeated, 1);
+    }
+  })
+  // do i need to favoriteValidate here?
+  // toggle hide/show 
+}
+
 
 // maybe should put a toggle on heart showing up and that belonging to the favorites array
 function addMealToFavorites(target) {
@@ -146,6 +174,7 @@ function addMealToFavorites(target) {
     return recipe.id == target.id
   })
   favoriteValidate(targetRecipe);
+  toggleFavorite(targetRecipe);
   // if(!user.favorites.includes(targetRecipe) 
   //   user.addToFavorites(targetRecipe)
   
@@ -153,6 +182,58 @@ function addMealToFavorites(target) {
 
 function favoriteValidate(targetRecipe) {
   !user.favorites.includes(targetRecipe) ? user.addToFavorites(targetRecipe) : null;
+}
+
+function toggleFavorite(targetRecipe) {
+  // run a find over the cards to find the one that contains/includes the event.target.id
+  // if it does not contain the active heart , then hide/show toggle
+  // that return get the affected 
+  let clickedIcon = event.target.id
+  let inactiveIcon = document.getElementsByClassName('favorite-icon-inactive')
+  // let formatted = inactiveIcon.reduce((acc, img) => {
+    
+  //   return acc
+  // }, '')
+  // let cardSelected = inactiveIcon.find(card => {
+  //   return  card.id === clickedIcon;
+  // }) 
+  
+
+
+
+  // grab icon 
+  // console.log('clicked thing', targetRecipe)
+  // let clickedIcon = event.target.id;
+  // console.log('three', clickedIcon)
+  // // let activeIcon = document.getElementsByClassName('favorite-icon-active')
+  // console.log('one', unfavorited);
+  // console.log('two', favorited);
+
+  // if (targetRecipe.id === clickedIcon) {
+  //   unfavorited.classList.add('hidden');
+  //   favorited.classList.remove('hidden');
+  // }
+
+  // let toggle = activeIcon.indexOf(clickedIcon)
+  // console.log('toggle', toggle)
+  // if (toggle > 0) {
+  //   inactiveIcon.classList.add('hidden')
+  //   activeIcon.classList.remove('hidden')
+  // }
+  // console.log("toggle", toggle)
+  // if (toggle === )
+
+
+
+  // if (clickedIcon === inactiveIcon.id) {
+  //   console.log(event.target);
+  //   inactiveIcon.classList.add('hidden')
+  //   activeIcon.classList.remove('hidden')
+  // } else {
+  //   activeIcon.classList.add('hidden')
+  //   inactiveIcon.classList.remove('hidden')
+  // }
+  // on click add hide to icon and toggle to other icon
 }
 
 
